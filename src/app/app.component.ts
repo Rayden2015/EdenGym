42e51c1dbaf294fe2a54cbd4ed014bc1127d9327
Nurudin Lartey <nurundin2010@gmail.com>
2021-10-24 16:55:47 +0000

    Sentry Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,11 @@
+//Sentry
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import * as Sentry from "@sentry/angular";
+import { Integrations } from "@sentry/tracing";
+
+import { AppModule } from '../app/app.module';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +13,34 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
+
+
+//enableProdMode();
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .then(success => console.log(`Bootstrap success`))
+//   .catch(err => console.error(err));
+
+
 export class AppComponent {
-  constructor() {}
+  constructor() {
+
+
+Sentry.init({
+  dsn: 'https://45d4de1958e042f7bd1870a052c4a5ae@o292934.ingest.sentry.io/6025542',
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ['localhost', 'https://edengym-7b791.web.app'],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,32 +16,6 @@ import { CommonModule } from '@angular/common';
 import { AngularFirePerformanceModule} from '@angular/fire/compat/performance';
 
 
-//Sentry
-//import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import * as Sentry from '@sentry/angular';
-import { Integrations } from '@sentry/tracing';
-//import { AppModule } from "./app/app.module";
-
-Sentry.init({
-  dsn: 'https://50d69d1d58a048fa9e4ad726b409de86@o292934.ingest.sentry.io/5177920' ,
-  integrations: [
-    // Registers and configures the Tracing integration,
-    // which automatically instruments your application to monitor its
-    // performance, including custom Angular routing instrumentation
-    new Integrations.BrowserTracing({
-      tracingOrigins: ['localhost', 'https://yourserver.io/api'],
-      routingInstrumentation: Sentry.routingInstrumentation,
-    }),
-  ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
-
-
 // platformBrowserDynamic()
 //   .bootstrapModule(AppModule)
 //   .then(success => console.log(`Bootstrap success`))
